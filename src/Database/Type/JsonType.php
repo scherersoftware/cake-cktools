2b050c25cc9b0353b3dd2782ee945db5dc1b0f85
Robert Scherer <ich@robertscherer.de>
2015-01-21 14:17:08 +0100

    added JsonType

---
+++ src/Database/Type/JsonType.php
@@ -0,0 +1,33 @@
+<?php 
+namespace CkTools\Database\Type;
+
+use Cake\Database\Driver;
+use Cake\Database\Type;
+
+class JsonType extends Type {
+
+/**
+ * from database to PHP conversion
+ * 
+ * @param string $value 	the value
+ * @param Driver $driver 	the driver
+ * @return array
+ */
+	public function toPHP($value, Driver $driver) {
+		if ($value === null) {
+			return null;
+		}
+		return json_decode($value, true);
+	}
+
+/**
+ * from PHP to database conversion
+ * 
+ * @param array|string $value 	the value
+ * @param Driver $driver 		the driver
+ * @return array
+ */
+	public function toDatabase($value, Driver $driver) {
+		return json_encode($value);
+	}
+}
