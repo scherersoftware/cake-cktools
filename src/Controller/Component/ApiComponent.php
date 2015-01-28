8a17cbff2f0cebb45b85b479a25852a7f1c8b960
Robert Scherer <ich@robertscherer.de>
2015-01-28 11:57:34 +0100

    ApiComponent response

--- src/Controller/Component/ApiComponent.php
+++ src/Controller/Component/ApiComponent.php
@@ -3,12 +3,22 @@ namespace CkTools\Controller\Component;
 
 use Cake\Controller\Component;
 use Cake\Controller\ComponentRegistry;
+use CkTools\Lib\ApiReturnCode;
 
 /**
  * Api component
  */
 class ApiComponent extends Component
 {
+
+    /**
+     * Used Components
+     *
+     * @var array
+     */
+    public $components = [
+        'RequestHandler'
+    ];
 
     /**
      * Default configuration.
@@ -18,11 +28,38 @@ class ApiComponent extends Component
     protected $_defaultConfig = [];
 
     /**
-     * Returns a standartized API response
+     * Should be called in the controller's beforeFilter callback
      *
+     * @return void
+     */
+    public function setup()
+    {
+        $this->RequestHandler->addInputType('json', ['json_decode', true]);
+        $this->RequestHandler->prefers('json');
+        // Force a JSON response regardless of extension
+        // $this->RequestHandler->renderAs($this->_registry->getController(), 'json');
+    }
+
+    /**
+     * Returns a standartized JSON response
+     *
+     * @param int $httpStatusCode HTTP Status Code to send
+     * @param string $returnCode A string code more specific to the result
+     * @param array $data Data for the 'data' key
      * @return Response
      */
-    public function response()
+    public function response($httpStatusCode = 200, $returnCode = ApiReturnCode::SUCCESS, array $data = [])
     {
+        $response = $this->_registry->getController()->response;
+        $response->statusCode($httpStatusCode);
+
+        $responseData = [
+            'code' => $returnCode,
+            'data' => $data
+        ];
+        $response->type('json');
+        $response->body(json_encode($responseData));
+
+        return $response;
     }
 }
---
+++ src/Lib/ApiReturnCode.php
@@ -0,0 +1,10 @@
+<?php
+namespace CkTools\Lib;
+
+class ApiReturnCode
+{
+    const SUCCESS = 'success';
+    const NOT_AUTHENTICATED = 'not_authenticated';
+    const INVALID_PARAMS = 'invalid_params';
+    const INVALID_CREDENTIALS = 'invalid_credentials';
+}
