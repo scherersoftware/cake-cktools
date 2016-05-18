7b754bc92c711b5eb5b1b0819aeea2a35215da6d
Robert Scherer <ich@robertscherer.de>
2016-05-18 11:15:28 +0200

    utility shell command for clearing data of all configured Cache engines

---
+++ src/Shell/CkToolsShell.php
@@ -0,0 +1,53 @@
+<?php
+namespace CkTools\Shell;
+
+use Cake\Cache\Cache;
+use Cake\Console\Shell;
+
+/**
+ * CkTools shell command.
+ */
+class CkToolsShell extends Shell
+{
+
+    /**
+     * Manage the available sub-commands along with their arguments and help
+     *
+     * @see http://book.cakephp.org/3.0/en/console-and-shells.html#configuring-options-and-generating-help
+     *
+     * @return \Cake\Console\ConsoleOptionParser
+     */
+    public function getOptionParser()
+    {
+        $parser = parent::getOptionParser();
+        return $parser->description([
+            'CkTools Shell',
+            '',
+            'Utilities'
+        ])->addSubcommand('clearCache', [
+            'help' => 'Clears data of all configured Cache engines.'
+        ]);
+        return $parser;
+    }
+
+    /**
+     * main() method.
+     *
+     * @return bool|int Success or error code.
+     */
+    public function main()
+    {
+        $this->out($this->OptionParser->help());
+    }
+
+    /**
+     * Clears data of all configured Cache engines.
+     *
+     * @return void
+     */
+    public function clearCache()
+    {
+        Cache::clearAll();
+        $this->out('All caches cleared');
+    }
+}
