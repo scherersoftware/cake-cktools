949f272faadb1ad8d27683439ddb4c3af4145185
Felix Kempf <fk@scherer-software.de>
2016-06-28 11:35:01 +0200

    add js component and controller trait for sortable behavior

---
+++ src/Utility/SortableControllerTrait.php
@@ -0,0 +1,36 @@
+<?php
+namespace CkTools\Utility;
+
+use Cake\Network\Exception\NotFoundException;
+use Cake\Orm\TableRegistry;
+use CkTools\Lib\ApiReturnCode;
+use FrontendBridge\Lib\ServiceResponse;
+
+/**
+ * This trait provides the endpoint for sortable lists.
+ * The controller using this trait MUST set a property $sortModelName with the name of the table to
+ * get and save the entity in question
+ */
+trait SortableControllerTrait {
+
+    /**
+     * endpoint for sorting ajax calls
+     *
+     * @return ServiceResponse
+     * @throws Cake\Network\Exception\NotFoundException if either not a post request or missing or wrong params
+     */
+    public function sort()
+    {
+        $this->request->allowMethod('post');
+        if (!empty($this->request->data['foreignKey']) && !empty($this->sortModelName)) {
+            $table = TableRegistry::get($this->sortModelName);
+            $entity = $table->get($this->request->data['foreignKey']);
+            $entity->sort = $this->request->data['sort'];
+            if ($table->save($entity)) {
+                return new ServiceResponse(ApiReturnCode::SUCCESS, [$entity->id, $this->request->data['sort']]);
+            }
+            return new ServiceResponse(ApiReturnCode::INTERNAL_ERROR, []);
+        }
+        throw new NotFoundException();
+    }
+}
---
+++ webroot/js/app/components/SortableComponent.js
@@ -0,0 +1,34 @@
+App.Components.SortableComponent = Frontend.Component.extend({
+    defaultConfig: null,
+    startup: function() {
+    },
+    initSortableLists: function($dom) {
+        $dom.find('table.sortable-list tbody').sortable().disableSelection();
+        $dom.find('table.sortable-list tbody').on("sortupdate", this._onSortUpdate.bind(this));
+    },
+    _onSortUpdate: function(e, ui) {
+        var foreignKey = ui.item.data('entity-id');
+        var sort = ui.item.index() + 1;
+
+        if (typeof(this.Controller._dom.find('table.sortable-list tbody').children()[sort-1]) == "undefined") {
+            sort = 1;
+        }
+        App.Main.UIBlocker.blockElement(this.Controller._dom);
+        App.Main.request(
+            {
+                plugin: this.Controller._frontendData.request.plugin,
+                controller: this.Controller._frontendData.request.controller,
+                action: 'sort',
+            }, {
+                foreignKey: foreignKey,
+                sort: sort
+            },
+            function(data) {
+                App.Main.UIBlocker.unblockElement(this.Controller._dom);
+                if (data.code == 'success') {
+                } else {
+                    alert("Error on update sort order! Please reload page.");
+                }
+            }.bind(this));
+    }
+});
