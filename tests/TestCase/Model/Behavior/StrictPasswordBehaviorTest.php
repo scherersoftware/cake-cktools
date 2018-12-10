e1c69e262cf3c1833375bcc088ee9932ab917d97
Felix Kempf <fk@scherer-software.de>
2018-12-10 17:06:09 +0100

    Internationalize test data and complete test cases

--- tests/TestCase/Model/Behavior/StrictPasswordBehaviorTest.php
+++ tests/TestCase/Model/Behavior/StrictPasswordBehaviorTest.php
@@ -29,7 +29,7 @@ class StrictPasswordBehaviorTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->Users = TableRegistry::get('CkTools.Users');
+        $this->Users = TableRegistry::getTableLocator()->get('CkTools.Users');
     }
 
     /**
@@ -40,8 +40,29 @@ class StrictPasswordBehaviorTest extends TestCase
     public function tearDown(): void
     {
         unset($this->Users);
-        TableRegistry::remove('CkTools.Users');
+        TableRegistry::getTableLocator()->remove('CkTools.Users');
         parent::tearDown();
+    }
+
+    public function testMinLength(): void
+    {
+        $this->Users->addBehavior('CkTools.StrictPassword', [
+            'minPasswordLength' => 10,
+        ]);
+
+        $newUser = $this->Users->newEntity([
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => 'TooShort1',
+        ]);
+        $this->assertArrayHasKey('minLength', $newUser->getError('password'));
+
+        $newUser2 = $this->Users->newEntity([
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => 'NotTooShortLuckily1',
+        ]);
+        $this->assertArrayNotHasKey('minLength', $newUser2->getError('password'));
     }
 
     /**
@@ -52,37 +73,52 @@ class StrictPasswordBehaviorTest extends TestCase
     public function testValidFormat(): void
     {
         $this->Users->addBehavior('CkTools.StrictPassword', [
-            'userNameFields' => [
-                'firstname' => 'forename',
-                'lastname' => 'surname',
-            ],
             'minPasswordLength' => 8,
         ]);
 
+        // No uppercase
         $newUser = $this->Users->newEntity([
-            'forename' => 'vorname',
-            'surname' => 'nachname',
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
             'password' => 'forename',
         ]);
-        
-        // No uppercase
         $this->assertArrayHasKey('passwordFormat', $newUser->getError('password'));
 
-        $data = ['password' => 'FORENAME'];
-        $result = $this->Users->patchEntity($newUser, $data); // No lowercase
+        // No lowercase
+        $newUser = $this->Users->newEntity([
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => 'FORENAME',
+
+        ]);
         $this->assertArrayHasKey('passwordFormat', $newUser->getError('password'));
 
-        $data = ['password' => 'Forename'];
-        $result = $this->Users->patchEntity($newUser, $data); // No numeric value
+        // No numeric value
+        $newUser = $this->Users->newEntity([
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => 'Forename',
+
+        ]);
         $this->assertArrayHasKey('passwordFormat', $newUser->getError('password'));
 
-        $data = ['password' => 'Forename1'];
-        $result = $this->Users->patchEntity($newUser, $data);  // No special char
+        // No special char
+        $newUser = $this->Users->newEntity([
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => 'Forename1',
+
+        ]);
         $this->assertArrayHasKey('passwordFormat', $newUser->getError('password'));
 
-        $data = ['password' => 'Forename1?'];
-        $result = $this->Users->patchEntity($newUser, $data);
-        $this->assertEmpty($result->getErrors());
+        // all good
+        $newUser = $this->Users->newEntity([
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => 'Forename1?',
+
+        ]);
+        $this->assertEmpty($newUser->getErrors());
     }
 
     /**
@@ -93,16 +129,12 @@ class StrictPasswordBehaviorTest extends TestCase
     public function testCheckForUsername(): void
     {
         $this->Users->addBehavior('CkTools.StrictPassword', [
-            'userNameFields' => [
-                'firstname' => 'forename',
-                'lastname' => 'surname',
-            ],
             'minPasswordLength' => 8,
         ]);
 
         $newUser = $this->Users->newEntity([
-            'forename' => 'Forename2?',
-            'surname' => 'Nachname2?',
+            'firstname' => 'Forename2?',
+            'lastname' => 'Surname2?',
             'password' => 'Forename2?',
         ]);
 
@@ -110,17 +142,17 @@ class StrictPasswordBehaviorTest extends TestCase
         $this->assertArrayHasKey('passwordNoUserName', $newUser->getError('password'));
 
         $data = [
-            'forename' => 'Forename2?',
-            'surname' => 'Nachname2?',
-            'password' => 'Nachname2?',
+            'firstname' => 'Forename2?',
+            'lastname' => 'Surname2?',
+            'password' => 'Surname2?',
         ];
         $result = $this->Users->patchEntity($newUser, $data);
         $this->assertArrayHasKey('passwordNoUserName', $newUser->getError('password'));
 
         $data = [
-            'forename' => 'Forename2?',
-            'surname' => 'Nachname2?',
-            'password' => 'Keinname1?',
+            'firstname' => 'Forename2?',
+            'lastname' => 'Surname2?',
+            'password' => 'Noname98?',
         ];
         $result = $this->Users->patchEntity($newUser, $data);
         $this->assertEmpty($result->getErrors());
@@ -131,22 +163,19 @@ class StrictPasswordBehaviorTest extends TestCase
      *
      * @return void
      */
-    public function testCheckLastPasswords($value='')
+    public function testCheckLastPasswords(): void
     {
         $hasher = new DefaultPasswordHasher();
         $this->Users->addBehavior('CkTools.StrictPassword', [
-            'userNameFields' => [
-                'firstname' => 'forename',
-                'lastname' => 'surname',
-            ],
+
             'minPasswordLength' => 8,
         ]);
 
         $newUser = $this->Users->newEntity([
-            'forename' => 'vorname',
-            'surname' => 'nachname',
-            'password' => $hasher->hash('Passwort1?'),
-            'last_passwords' => [$hasher->hash('Passwort2?')],
+            'firstname' => 'Mike',
+            'lastname' => 'Jagger',
+            'password' => $hasher->hash('Password1?'),
+            'last_passwords' => [$hasher->hash('Password2?')],
         ]);
 
         $result = $this->Users->save($newUser);
@@ -154,18 +183,61 @@ class StrictPasswordBehaviorTest extends TestCase
 
         $data = [
             'id' => $newUser->id,
-            'password' => 'Passwort2?',
+            'password' => 'Password2?',
         ];
-        
+
         $result = $this->Users->patchEntity($newUser, $data);
         $this->assertArrayHasKey('passwordUsedBefore', $newUser->getError('password'));
 
         $data = [
             'id' => $newUser->id,
-            'password' => 'Passwort3?',
+            'password' => 'Password3?',
         ];
 
         $result = $this->Users->patchEntity($newUser, $data);
         $this->assertEmpty($result->getErrors());
+    }
+
+    public function testConfigurability(): void
+    {
+        $this->Users->addBehavior('CkTools.StrictPassword', [
+            'userNameFields' => [
+                'firstname' => 'qwert',
+                'lastname' => 'vbnm',
+            ],
+            'minPasswordLength' => 3,
+            'specialChars' => false,
+            'upperCase' => false,
+            'lowerCase' => false,
+            'numericValue' => false,
+        ]);
+
+        $newUser = $this->Users->newEntity([
+            'qwert' => 'Mike',
+            'vbnm' => 'Jagger',
+            'password' => 'foo',
+        ]);
+        $this->assertEmpty($newUser->getErrors());
+
+        $newUser = $this->Users->newEntity([
+            'qwert' => 'Mike',
+            'vbnm' => 'Jagger',
+            'password' => 'BAR',
+        ]);
+        $this->assertEmpty($newUser->getErrors());
+
+        $newUser = $this->Users->newEntity([
+            'qwert' => 'Mike',
+            'vbnm' => 'Jagger',
+            'password' => '123',
+        ]);
+        $this->assertEmpty($newUser->getErrors());
+
+        $newUser = $this->Users->newEntity([
+            'qwert' => 'Mike',
+            'vbnm' => 'Jagger',
+            'password' => '?=&',
+        ]);
+        $this->assertEmpty($newUser->getErrors());
     }
 }
